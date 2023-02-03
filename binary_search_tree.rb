bb6de62e923dc31386132d8f04e81be5386ddfc0
Valoeable <samo.valo3@gmail.com>
2023-02-03 13:12:10 +0100

    Added method for creation of a binary search tree and a method for printing out the tree.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,10 +1,10 @@
 class Node
     attr_accessor :data, :left, :right
 
-    def initialize(data, left, right)
+    def initialize(data)
         @data = data
-        @left = left
-        @right = right
+        @left = nil
+        @right = nil
     end
 end
 
@@ -12,10 +12,32 @@ class Tree
     attr_accessor :root
 
     def initialize(array)
-        @root = build_tree(array)
+        sorted_array = array.sort.uniq
+        @root = build_tree(sorted_array)
     end
 
     def build_tree(array)
 
+        return nil if array.empty?
+
+        mid = (array.length - 1) / 2
+        new_node = Node.new(array[mid])
+        new_node.left = build_tree(array[0...mid])
+        new_node.right = build_tree(array[(mid + 1)..-1])
+
+        new_node
+
     end
+
+    def pretty_print(node = root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
 end
+
+array = Array.new(15) { rand(1..100) }
+bst = Tree.new(array)
+
+bst.pretty_print
